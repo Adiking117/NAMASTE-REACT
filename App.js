b81bc9f50fad0b5194bc7917498931a1c11e6d3a
Adiking117 <www.aditya117@gmail.com>
2023-12-31 16:25:16 +0530

    Lecture - 4 (Dynamic Details)

--- App.js
+++ App.js
@@ -21,29 +21,140 @@ const Header = () => {
     )
 }
 
-// const RestaurantCard = (props) => {
-//     console.log(props)
-//     return (
-//         <div className='res-card'>
-//             <img className='res-logo' src={rescard}/>
-//             <h3>{props.resname}</h3> 
-//             <h4>{props.cuisine}</h4>   
-//             <h4>{props.rating}</h4> 
-//             <h4>{props.deltime}</h4>       
-//         </div>
-//     )
-// }
+const resObj = {
+    type: "restaurant",
+    data: {
+      type: "F",
+      id: "74453",
+      name: "Domino's Pizza",
+      uuid: "87727dbd-7f2b-4857-9763-359624165845",
+      city: "21",
+      area: "Athwa",
+      totalRatingsString: "1000+ ratings",
+      cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+      cuisines: ["Pizzas"],
+      tags: [],
+      costForTwo: 40000,
+      costForTwoString: "₹400 FOR TWO",
+      deliveryTime: 45,
+      minDeliveryTime: 45,
+      maxDeliveryTime: 45,
+      slaString: "45 MINS",
+      lastMileTravel: 0,
+      slugs: {
+        restaurant: "dominos-pizza-majura-nondh-test_surat",
+        city: "surat",
+      },
+      cityState: "21",
+      address:
+        "Shop 32 To 35, Sheetal Shopping Square,Near Lb Turning Point, Bhatar Road,MAJURA NONDH, Surat,GUJARAT-395001",
+      locality: "Bhatar Road",
+      parentId: 2456,
+      unserviceable: false,
+      veg: true,
+      select: false,
+      favorite: false,
+      tradeCampaignHeaders: [],
+      aggregatedDiscountInfo: {
+        header: "FREE DELIVERY",
+        shortDescriptionList: [
+          {
+            meta: "FREE DELIVERY",
+            discountType: "FREE_DELIVERY",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "FREE DELIVERY",
+            discountType: "FREE_DELIVERY",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      aggregatedDiscountInfoV2: {
+        header: "",
+        shortDescriptionList: [
+          {
+            meta: "Free Delivery",
+            discountType: "FREE_DELIVERY",
+            operationType: "RESTAURANT",
+          },
+        ],
+        descriptionList: [
+          {
+            meta: "FREE DELIVERY",
+            discountType: "FREE_DELIVERY",
+            operationType: "RESTAURANT",
+          },
+        ],
+        subHeader: "",
+        headerType: 0,
+        superFreedel: "",
+      },
+      chain: [],
+      feeDetails: {
+        fees: [],
+        totalFees: 0,
+        message: "",
+        title: "",
+        amount: "",
+        icon: "",
+      },
+      availability: {
+        opened: true,
+        nextOpenMessage: "",
+        nextCloseMessage: "",
+      },
+      longDistanceEnabled: 0,
+      rainMode: "NONE",
+      thirdPartyAddress: false,
+      thirdPartyVendor: "",
+      adTrackingID: "",
+      badges: {
+        imageBased: [],
+        textBased: [],
+        textExtendedBadges: [],
+      },
+      lastMileTravelString: "2.1 kms",
+      hasSurge: false,
+      sla: {
+        restaurantId: "74453",
+        deliveryTime: 45,
+        minDeliveryTime: 45,
+        maxDeliveryTime: 45,
+        lastMileTravel: 0,
+        lastMileDistance: 0,
+        serviceability: "SERVICEABLE",
+        rainMode: "NONE",
+        longDistance: "NOT_LONG_DISTANCE",
+        preferentialService: false,
+        iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "4.0",
+      totalRatings: 1000,
+      new: false,
+    },
+    subtype: "basic",
+}
 
 const RestaurantCard = (props) => {
-    const { resname,cuisine,rating,deltime } = props
-    // console.log(props)
+    const { resData } = props
     return (
         <div className='res-card'>
-            <img className='res-logo' src={rescard}/>
-            <h3>{resname}</h3> 
-            <h4>{cuisine}</h4>   
-            <h4>{rating}</h4> 
-            <h4>{deltime}</h4>       
+            <img className='res-logo' src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          resData.data.cloudinaryImageId
+        }/>
+            <h3>{resData.data.name}</h3> 
+            <h4>{resData.data.cuisines}</h4>
+            <h4>{resData.data.deliveryTime}</h4> 
+            <h4>{resData.data.avgRating}</h4>   
+            <h4>{resData.data.costForTwo/100}</h4>       
         </div>
     )
 }
@@ -55,8 +166,7 @@ const Body = () => {
                 Search
             </div>
             <div className='res-container'>
-                <RestaurantCard resname="KFC" cuisine="Fast Food" rating="4.5 Stars" deltime="30 minutes"/>
-                <RestaurantCard resname="Burger King" cuisine="Burger" rating="4.2 Stars" deltime="35 minutes"/>
+                <RestaurantCard resData={resObj}/>
             </div>
         </div>
     )
